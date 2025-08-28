910738b584a66c95fed94913ecbae0c5f5c6017e
mareeb16 <mohammadareeb1000@gmail.com>
2025-08-28 17:24:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,172 @@
+// console.log("JS Connected")
+
+
+// 1.
+// alert("My name is M.Areeb")
+
+
+// 2.
+// var userName = "Areeb"
+// console.log(userName);
+
+
+// 3.
+// var Number = "2"
+// console.log(Number)
+
+
+// 4.
+// legal
+// var userName = "M.Areeb"
+// console.log(userName)
+
+// illegal
+// var 2name = "areeb"
+// console.log(2name)
+
+// 5.
+// // familiar operators
+// var result = 5 + 3; 
+// console.log(result)
+
+
+// var result = 5 - 3; 
+// console.log(result)
+
+
+// var result = 5 * 3; 
+// console.log(result)
+
+
+// var result = 5 / 3; 
+// console.log(result)
+
+// 6.
+// unfamiliar operators
+
+    // var a = 10; // Assigns 10 to a
+    // a += 5; // Equivalent to a = a + 5; a is now 15
+    // console.log(a)
+
+    // 7.
+    // var result1 = 3 + 2 * 4; 
+// console.log(result1); 
+
+// 8.
+// var firstName = "Mohammad";
+// var lastName = "Areeb";
+
+// var fullName = firstName + " " + lastName;
+// console.log(fullName); 
+
+// 9.
+// var userName = prompt("What is your name?");
+// console.log("Hello " + userName);
+
+// 10.
+// var age = 18;
+
+// if (age >= 18) {
+//   console.log("You are an adult.");
+// }
+
+// 11.
+// 5 == "5";   
+// 5 === "5";  
+// 5 === 5;   
+// 10 != "10"; 
+// 10 != 5;    
+
+
+// 12.
+// var marks = 75;
+
+// if (marks >= 90) {
+//   console.log("Grade: A");
+// } else if (marks >= 60) {
+//   console.log("Grade: B");
+// } else {
+//   console.log("Grade: C");
+// }
+
+
+// 13.
+// var age = 20;
+// var hasID = true;
+
+// if (age >= 18 && hasID) {
+//   console.log("You can enter!");
+// } else {
+//   console.log("Access denied!");
+// }
+
+// 14.
+// var num = 15;
+
+// if (num > 0) {
+//   if (num % 2 === 0) {
+//     console.log("Positive Even");
+//   } else {
+//     console.log("Positive Odd");
+//   }
+// }
+
+
+// 15.
+// var fruits = ["Apple", "Banana", "Mango"];
+// console.log(fruits[0]); // Apple
+
+// 16.
+// let colors = ["Red", "Green"];
+// colors.push("Blue");   // Add at end
+// colors.unshift("Yellow"); // Add at start
+// colors.pop();          // Remove last
+// colors.shift();        // Remove first
+
+// console.log(colors); // ["Green"]
+
+// 17.
+// var numbers = [10, 20, 30, 40, 50];
+
+// Remove 1 element at index 2
+// numbers.splice(2, 1);
+
+// Insert at index 1
+// numbers.splice(1, 0, 15);
+
+// Extract slice (copy from index 1 to 3)
+// var newArr = numbers.slice(1, 3);
+
+// console.log(numbers); // [10, 15, 20, 40, 50]
+// console.log(newArr);  // [15, 20]
+
+// // 18.
+// for (var i = 1; i <= 5; i++) {
+//   console.log("Number: " + i);
+// }
+
+// // 19.
+// var items = [2, 4, 6, 8, 10];
+// var found = false;
+
+// for (var i = 0; i < items.length; i++) {
+//   if (items[i] === 6) {
+//     found = true;
+//     console.log("Found 6 at index " + i);
+//     break; // exit loop
+//   }
+// }
+
+// if (!found) {
+//   console.log("6 not found!");
+// }
+
+
+// // 20.
+// for (var i = 1; i <= 3; i++) {
+//   for (var j = 1; j <= 2; j++) {
+//     console.log(`i=${i}, j=${j}`);
+//   }
+// }
+
+
